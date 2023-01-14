ac8821d9df99867c50a13d0253e49e919bfcadcd
Vincent Tavernier <v.tavernier@pm.me>
2023-01-14 18:37:03 +0100

    ci: switch to github-workflows@v1

---
+++ ci/release.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+set_version () {
+    # Version specifications in Cargo.toml/lock
+    sed -i '/transmission-proxy\|transmission-rpc-client/{n;s/^version = "[^"]*"/version = "'"$1"'"/}' crates/*/Cargo.toml Cargo.lock
+
+    # Dependency to transmission-rpc-client
+    sed -i '/transmission-rpc-client\s*=/{s/version = "[^"]*"/version = "'"$1"'"/}' crates/transmission-proxy/Cargo.toml Cargo.lock
+}
+
+action="$1"
+shift
+
+case "$action" in
+    set-version)
+        set_version "$@"
+        ;;
+    *)
+        echo "Usage: ./ci/release.sh set-version|publish"
+        ;;
+esac
